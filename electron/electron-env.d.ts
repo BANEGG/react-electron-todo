518e20f231e2293c41164a16615fb9e7188af118
BANE <banelab.gg@gmail.com>
2023-06-06 23:36:50 +0300

    init

---
+++ electron/electron-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="vite-plugin-electron/electron-env" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    /**
+     * The built directory structure
+     *
+     * ```tree
+     * ├─┬─┬ dist
+     * │ │ └── index.html
+     * │ │
+     * │ ├─┬ dist-electron
+     * │ │ ├── main.js
+     * │ │ └── preload.js
+     * │
+     * ```
+     */
+    DIST: string
+    /** /dist/ or /public/ */
+    PUBLIC: string
+  }
+}
+
+type Todo = {
+  id: number,
+  name: string,
+  description: string,
+  checked: boolean
+}
